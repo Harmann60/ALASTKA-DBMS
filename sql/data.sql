42d10b40c1ee6aef2493e295b5611087099e3285
Harman <harmanjss10@gmail.com>
2026-03-29 12:17:56 +0530

    Create data.sql

---
+++ sql/data.sql
@@ -0,0 +1,17 @@
+USE ALASTKA;
+
+INSERT INTO Country VALUES
+(1, 'India'),
+(2, 'Italy'),
+(3, 'Japan'),
+(4, 'Finland');
+
+INSERT INTO City VALUES
+(1, 'Delhi', 1),
+(2, 'Rome', 2),
+(3, 'Tokyo', 3),
+(4, 'Helsinki', 4);
+
+INSERT INTO Users(Name, Email, Password) VALUES
+('Harman', 'harman@gmail.com', '123'),
+('Ayush', 'ayush@gmail.com', '123');
